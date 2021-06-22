16ec7842f798121005cef1c69c6193a7d0a9e239
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-23 00:53:35 +0900

    Rendering aMap with Google Maps (incl. Types!) with dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
-import { GOOGLE_API_KEY } from '../API_KEYS';
+
+const script = document.createElement('script');
+script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAP_API_KEY}`;
+document.head.append(script);
 
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
@@ -17,13 +20,25 @@ function searchAddressHandler(event: Event) {
     .get<GoogleGeocodingResponse>(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
-      )}&key=${GOOGLE_API_KEY}`
+      )}&key=${process.env.GOOGLE_MAP_API_KEY}`
     )
     .then(response => {
       if (response.data.status !== 'OK') {
         throw new Error('Could not fetch location!');
       }
       const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(
+        document.getElementById('map') as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        }
+      );
+
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
     })
     .catch(err => {
       alert(err.message);
